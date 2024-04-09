1dbebb499cc045074a22a27d7677186676412d91
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-04-09 15:57:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -52,3 +52,20 @@ variable "private_subnet_b_cidr_blocks" {
   type        = string
   description = "private subnet 1b CIDR blocks"
 }
+
+# Define variables
+variable "instance_sg_name" {
+  description = "The name of the sg "
+}
+
+variable "ami" {
+  description = "ami id "
+}
+
+variable "instance_type" {
+  description = "instance type"
+}
+
+variable "key_pair" {
+  description = "keypair for server "
+}
