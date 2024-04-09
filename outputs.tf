00b87963cba3dcfe97de8e3eaecc3c7011bd9270
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-04-09 16:02:12 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -28,10 +28,3 @@ output "cidr_block" {
   description = "The CIDR block associated with the VPC"
 }
 
-output "public_ip" {
-  value = aws_instance.jumpbox.public_ip
-}
-
-output "instance_id" {
-  value = aws_instance.jumpbox.id
-}
