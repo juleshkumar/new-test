706fce3284d3b38bea3fda6f808b47bda5265c44
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-04-12 19:00:33 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,30 @@
 # Define variables
 variable "instance_sg_name" {
+  type        = string
   description = "The name of the sg "
 }
 
 variable "ami" {
+  type        = string
   description = "ami id "
 }
 
 variable "instance_type" {
+  type        = string
   description = "instance type"
 }
 
 variable "key_pair" {
+  type        = string
   description = "keypair for server "
 }
 
 variable "subnet_id" {
+  type        = string
   description = "subnet id from above stage"
 }
 
 variable "vpc_id" {
+  type        = string
   description = "vpc id from above stage "
 }
