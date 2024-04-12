a788739dec43b1de35370285a82230ec4edc9051
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-04-12 14:44:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,3 +14,11 @@ variable "instance_type" {
 variable "key_pair" {
   description = "keypair for server "
 }
+
+variable "subnet_id" {
+  description = "subnet id from above stage"
+}
+
+variable "vpc_id" {
+  description = "vpc id from above stage "
+}
