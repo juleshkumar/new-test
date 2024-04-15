f02079a702931d063838a8072dd5c86f833c45e3
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-04-15 10:57:13 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,9 @@
-provider "aws" {
-  region = "ap-south-1"
+terraform {
+  required_version = "~> 1.8.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+    }
+  }
 }
