9b4d1545096b783452db027ee57c3c7f8031d89b
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-04-16 12:13:09 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,6 +9,16 @@ variable "region" {
   description = "aws region "
 }
 
+variable "access_key" {
+  type        = string
+  description = "aws access key "
+}
+
+variable "secret_key" {
+  type        = string
+  description = "aws secret key "
+}
+
 variable "ami" {
   type        = string
   description = "ami id "
