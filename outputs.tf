ecde4404217d584fa4abc8b2966ef65c1e26c75f
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-04-26 17:13:21 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,11 @@ output "efs_file_system_id" {
   value = aws_efs_file_system.efs.id
 }
 
-output "efs_mount_target_1.id" {
+output "mount_target_1_id" {
   value = aws_efs_mount_target.mount_target_1.id
 }
 
-output "efs_mount_target_2.id" {
+output "mount_target_2_id" {
   value = aws_efs_mount_target.mount_target_2.id
 }
 
