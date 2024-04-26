41d22d487a67b618ebf7a878cd6e97dfeb854008
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-04-26 14:41:14 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,12 @@
-output "efs_id" {
-  value = aws_efs_file_system.example.id
+output "efs_file_system_id" {
+  value = aws_efs_file_system.efs.id
 }
 
-output "mount_target_ips" {
-  value = aws_efs_mount_target.example[*].ip_address
+output "efs_mount_target_1.id" {
+  value = aws_efs_mount_target.mount_target_1.id
 }
+
+output "efs_mount_target_2.id" {
+  value = aws_efs_mount_target.mount_target_2.id
+}
+
