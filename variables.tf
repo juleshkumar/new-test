680d0cf0e64de7b50f717a34aa46d6a981f61bc1
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-04-26 14:39:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,24 @@
 variable "vpc_id" {
   description = "The ID of the VPC where you want to create the EFS"
+  type        = string
 }
 
 variable "efs_name" {
   description = "Name for the Elastic File System"
-  default     = "my-efs"
+  type        = string
 }
 
-variable "subnet_ids" {
-  type        = list(string)
-  description = "List of Subnet IDs in which to create the mount targets"
+variable "sub1_id" {
+  type        = string
+  description = "List of Subnet 1 IDs in which to create the mount targets"
 }
 
-variable "security_group_ids" {
-  type        = list(string)
-  description = "List of Security Group IDs to associate with the EFS"
+variable "sub2_id" {
+  type        = string
+  description = "List of Subnet 2 IDs in which to create the mount targets"
+}
+
+variable "region" {
+  type        = string
+  description = "aws region "
 }
