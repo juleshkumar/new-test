92a0af2b70b8cc5d54cf2f074c77a31dc468c721
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-04-30 16:05:14 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,11 @@
 output "instance_public_ip" {
-  description = "Public IP of EC2 instance"
-  value       = aws_instance.jumpbox.public_ip
+  value = aws_instance.jumpbox.public_ip
+}
+
+output "instance_private_ip" {
+  value = aws_instance.jumpbox.private_ip
+}
+
+output "security_group_id" {
+  value = aws_security_group.jumpbox_sg.id
 }
